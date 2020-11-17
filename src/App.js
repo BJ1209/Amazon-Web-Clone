71448145065281754df24d39a817df1182180cb7
Bikram Jeet Singh <me.bikram.dev@gmail.com>
2020-11-17 17:45:48 +0530

    Added The Checkout Page Functionality and Routing to the App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
 import Header from "./components/Header";
 import Home from "./components/Home";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
+import Checkout from "./components/Checkout";
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <Home />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/checkout">
+          <Checkout />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,14 @@ import React from "react";
 import "./Header.css";
 import amazonLogo from "./../images/amazon-ar21.svg";
 import * as MDIcons from "react-icons/md";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <div className="header">
-      <img className="header__logo" src={amazonLogo} alt="Amazon Logo" />
+      <Link to="/">
+        <img className="header__logo" src={amazonLogo} alt="Amazon Logo" />
+      </Link>
       <div className="header__search">
         <input className="header__searchInput" type="text" />
         <MDIcons.MdSearch className="header__searchIcon" />
@@ -24,10 +27,12 @@ function Header() {
           <span className="header__optionLineOne">Your</span>
           <span className="header__optionLineTwo">Prime</span>
         </div>
-        <div className="header__optionBasket">
-          <MDIcons.MdShoppingBasket className="header__basketIcon" />
-          <span className="header__optionLineTwo header__basketCount">0</span>
-        </div>
+        <Link to="/checkout">
+          <div className="header__optionBasket">
+            <MDIcons.MdShoppingBasket className="header__basketIcon" />
+            <span className="header__optionLineTwo header__basketCount">0</span>
+          </div>
+        </Link>
       </div>
     </div>
   );
